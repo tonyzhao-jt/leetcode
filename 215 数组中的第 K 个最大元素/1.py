43b2b9c0b4b683f48e505e6b084b9249a8e19e75
TonyZhao <35594984+SpringWave1@users.noreply.github.com>
2020-11-20 11:00:10 +0800

    Add files via upload

---
+++ 215 数组中的第 K 个最大元素/1.py
@@ -0,0 +1,38 @@
+class Solution:
+    def partition(self, arr, l, r):
+        if l > r:
+            return None
+        pivot = arr[r]
+        i = l
+        j = r
+
+        while i < j:
+            while i < j and arr[i] <= pivot:
+                i += 1
+            while i < j and arr[j] >= pivot:
+                j -= 1
+            if i > j:
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[i], arr[r] = arr[r], arr[i]
+        return i
+    def getLeastNumbersK(self, arr, l, r, k):
+        idx = self.partition(arr, l, r)
+        if idx == k:
+            return arr[idx]
+        elif idx < k:
+            return self.getLeastNumbersK(arr, idx + 1, r, k)
+        else:
+            return self.getLeastNumbersK(arr, l, idx - 1, k)
+
+    def getLeastNumbers(self, arr, k):
+        l = 0
+        r = len(arr) - 1
+        val = self.getLeastNumbersK(arr, l, r, k)
+        return val
+
+
+arr = [1,3,2,5,5,6]
+k = 3
+
+sol = Solution()
+print(sol.getLeastNumbers(arr, k))
